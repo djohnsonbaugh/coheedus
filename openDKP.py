95893cacdadaf2a7f414dea2e6fe8cffd5a26b7b
BigRax <smkalmes@gmail.com>
2022-07-29 23:35:39 -0500

    Add loadRaidTemplate call
    Add loadRaidTemplate call to openDKP.py

--- openDKP.py
+++ openDKP.py
@@ -1,3 +1,4 @@
+from urllib import response
 import requests, json
 from appConfig import appConfig
 from enum import Enum
@@ -7,8 +8,9 @@ class oDKPURL(Enum):
 class openDKP(object):
     """description of class"""
     def __init__(self, conf:appConfig):
-        self.clientID = conf.get("OPENDKP","clientid","")
+        self.clientID = conf.get("OPENDKP","clientid","") 
         self.urls :[str] = []
+        self.raidTemplateName = conf.get("OPENDKP","raidtemplate","") 
         for url in oDKPURL:
             self.urls.append(conf.get("OPENDKPAPIURLS",url.name,""))
         self.session = None
@@ -29,3 +31,21 @@ class openDKP(object):
             if(row["CharacterName"] == name):
                 return row["CurrentDKP"]
         return
+
+    def loadRaidTemplate(self):
+        url = "https://4jmtrkwc86.execute-api.us-east-2.amazonaws.com/beta/admin/settings/raid_templates"
+
+        responseString = json.loads(self.session.get(url).text)["SettingValue"]
+      
+        jsonTemplates = json.loads(responseString)["Templates"]
+        for row in jsonTemplates:
+            if(row["Name"] == self.raidTemplateName):
+                return row
+        return
+
+
+
+
+# Item Lookup
+#https://72rv4f6y1f.execute-api.us-east-2.amazonaws.com/beta/items/autocomplete?item=Helm of flowing&limit=5&game=0
+    
