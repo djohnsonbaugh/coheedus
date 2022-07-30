6e27e07b2abf09b568e2a34f19a9aaf46a40f242
Ryan Johnsonbaugh <ryan.johnsonbaugh@gmail.com>
2022-07-30 10:39:54 -0400

    Add project files.(without ini's/env's)

---
+++ eqLog.py
@@ -0,0 +1,58 @@
+import appConfig
+from os.path import exists
+import os
+import time
+from datetime import datetime
+import asyncio
+
+class eqLog(object):
+    """description of class"""
+
+    def __init__(self, conf:appConfig):
+        self.character = conf.get("EVERQUEST","character","coheedus")
+        self.server = conf.get("EVERQUEST","server","thornblade")
+        self.eqDir = conf.get("EVERQUEST","eqdir","C:\Everquest")
+        return
+
+    def getFileName(self, mod:str = "") ->str:
+        return self.eqDir + "\Logs\eqlog_" + self.character + "_" + self.server + mod + ".txt"
+
+    def resetLogFile(self):
+        timestamp:str = datetime.now().strftime("%Y%m%d%H%M%S")
+        if exists(self.getFileName()):
+            os.rename(self.getFileName(),self.getFileName(timestamp))
+        with open(self.getFileName(), "w") as f:
+            f.write("Log file reset at " + timestamp + ".\n")
+        return
+
+    def follow(file, sleep_sec=0): #-> Iterator[str]:**
+        """ Yield each line from a file as they are written.
+        `sleep_sec` is the time to sleep after empty reads. """
+        line = ''
+        while True:
+            tmp = file.readline()
+            if tmp is not None:
+                line += tmp
+                if line.endswith("\n"):
+                    yield line
+                    line = ''
+            else: 
+                if sleep_sec > 0:
+                    time.sleep(sleep_sec)
+
+    async def monitorLog(self, newLineFunc):
+        self.resetLogFile()
+        with open(self.getFileName(), 'r') as file:
+            line:str = ''
+            while True:
+                tmp = file.readline()
+                if tmp is not None:
+                    line += tmp
+                    if line.endswith("\n"):
+                        newLineFunc(line)
+                        line = ''
+                    else:
+                        await asyncio.sleep(0)
+                else: 
+                    await asyncio.sleep(0)
+        return
