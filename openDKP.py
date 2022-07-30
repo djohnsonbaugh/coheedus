4a142a31c8995af19b355d616cf396e84bc999f5
DeWayne Johnsonbaugh <djohnsonbaugh@gmail.com>
2022-07-29 20:46:40 -0400

    openDKP working kinda

--- openDKP.py
+++ openDKP.py
@@ -1,20 +1,31 @@
 import requests, json
 from appConfig import appConfig
+from enum import Enum
+class oDKPURL(Enum):
+    Summary = 0
 
 class openDKP(object):
     """description of class"""
     def __init__(self, conf:appConfig):
         self.clientID = conf.get("OPENDKP","clientid","")
-        self.server = conf.get("OPENDKPAPIURLS","summary","")
+        self.urls :[str] = []
+        for url in oDKPURL:
+            self.urls.append(conf.get("OPENDKPAPIURLS",url.name,""))
         self.session = None
-        createNewSession()
+        self.createNewSession()
         return
+
+
 
     def createNewSession(self):
         self.session = requests.Session()
         self.session.headers.update({"clientid":self.clientID})
+        return
 
 
-    def getDKP(self):
-
+    def getDKP(self, name:str):
+        trashjson = json.loads(self.session.get(self.urls[oDKPURL.Summary.value]).text)["Models"]
+        for row in trashjson:
+            if(row["CharacterName"] == name):
+                return row["CurrentDKP"]
         return
