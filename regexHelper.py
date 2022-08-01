f47efd1c0efe550a0fd846de19c9bb2015e5d823
Ryan Johnsonbaugh <ryan.johnsonbaugh@gmail.com>
2022-07-31 20:28:11 -0400

    regex patters for dkp,auc and bid commands

--- regexHelper.py
+++ regexHelper.py
@@ -1,7 +1,11 @@
 import re
 from botCommand import botCommand
-commandPattern = re.compile("(?x)\[[A-Za-z0-9: ]+\]\s([a-zA-Z]+)\stells\s(the\s)?([a-zA-Z]+:)?(group|guild|raid|you|\d+),\s'\w*!(.*)'")
-aucIDPattern = re.compile("(\d+)(\w+.*)?")
+commandPattern = re.compile("\[[\w\d: ]+\]\s(\w+)\stells\s(the\s)?(\w+:)?(group|guild|raid|you|\d+),\s'\w*!(.*)'")
+dkpCmdPtrn = re.compile("^\s*dkp\s+(?<toon>\w+)")
+aucCmdPtrn = re.compile("^\s*auc\s+((<switchOpts>-[sa]+)\s*)?((?<items>[^\n\r@]+[^\s@])\s*)(@\s*(?<duration>\d+)(\s+(?<quanity>\d+))?)?")
+aucIdCmdsPtrn = re.compile("^\s*(?<bidId>\d+)\s+(?<cmdType>pause|close|start|award)\s*(?<length>\d+)?")
+bidWithIDPtrn = re.compile("^\s*(?<bidId>\d+)\s+(?<bidVal>\d+)(\s+(?<bidMax>\d+))?(\s+(?<bidInc>-?[\d]+))?(\s*(?<proxyToon>\w+))?")
+bidWithItemPtrn = re.compile("^\s*(?<bidItem>[^\n\r@]+?)\s*@\s*(?<bidVal>\d+)(\s+(?<bidMax>\d+))?(\s+(?<bidInc>-?[\d]+))?(\s*(?<proxyToon>\w+))?")
 
 def isCommand(line:str, cmd: botCommand) -> bool:
     if(commandPattern.match(line)):
