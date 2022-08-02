c27eb7f618126e7cd392295d4d4c74b130c58178
DeWayne Johnsonbaugh <djohnsonbaugh@gmail.com>
2022-08-01 23:47:35 -0400

    added channel properties to auctioneer

--- aucitoneer.py
+++ aucitoneer.py
@@ -4,3 +4,13 @@ class Aucitoneer(object):
     def __init__(self,adminchan:str, auctionchan:str):
         self.__adminchan = adminchan
         self.__aucchan = auctionchan
+
+    @property
+    def AdminChannel(self)->str: return self.__adminchan
+    @AdminChannel.setter
+    def AdminChannel(self,value:str): self.__adminchan = value
+
+    @property
+    def AuctionChannel(self)->str: return self.__aucchan
+    @AdminChannel.setter
+    def AuctionChannel(self,value:str): self.__aucchan = value
