e32d42231e36471edd4c32aeb0a70647b20403f2
Ryan Johnsonbaugh <ryan.johnsonbaugh@gmail.com>
2022-08-01 22:42:14 -0400

    fixed some bugs some still exist

--- bot.py
+++ bot.py
@@ -9,21 +9,22 @@ def initConfig(config: appConfig):
     oDKP = openDKP(config)
     return
 
-AdminChannel = ""
-AuctionChannel = "rsay"
+AdminChannel    = ""
+AuctionChannel  = "rsay"
 
 # NORMAL COMMANDS
 def exBid(cmd: botCommand) ->str:
-    id = cmd.regMatch.group("bidId") if 'bidId' in cmd.regMatch.groupdict() else -1
-    item = cmd.regMatch.group("bidItem") if 'bidItem' in cmd.regMatch.groupdict() else ""
-    bidVal = cmd.regMatch.group("bidVal")
-    bidMax = cmd.regMatch.group("bidMax")
-    bidInc = cmd.regMatch.group("bidInc")
-    proxyToon = cmd.regMatch.group("proxyToon")
+    id          = int(cmd.regMatch.group("bidId"))      if 'bidId' in cmd.regMatch.groupdict() else -1
+    item        =     cmd.regMatch.group("bidItem")     if 'bidItem' in cmd.regMatch.groupdict() else ""
+    name        = cmd.Sender
+    bidVal      = int(cmd.regMatch.group("bidVal"))
+    bidMax      = int(cmd.regMatch.group("bidMax"))     if cmd.regMatch.groupdict("bidMax")     is not None else bidVal
+    bidInc      = int(cmd.regMatch.group("bidInc"))     if cmd.regMatch.group("bidInc")         is not None else 1
+    proxyToon   =     cmd.regMatch.group("proxyToon")   if cmd.regMatch.groupdict("proxyToon")  is not None else ""
     if id > 0:
-        return "Not Implemented but I if it did you would have bid on auction:" + id
+        return "Auctions do not exist but I if they did you would have bid on auction:" + id.__str__()
     else:
-        return "Not Implemented but I if it did you would have bid on item:" + item
+        return "Auctions do not exist but I if it did you would have bid on item:" + item
 
 def exAdmin(cmd: botCommand) -> str:
     global AdminChannel
@@ -31,26 +32,26 @@ def exAdmin(cmd: botCommand) -> str:
     return "Admin Commands & Messages Here"
 
 def exDKP(cmd: botCommand) -> str:
-    name = cmd.Params[1] if cmd.ParCount > 0 else cmd.Sender
-    dkp = oDKP.getDKP(name)
+    name        = cmd.Params[1] if cmd.ParCount > 0 else cmd.Sender
+    dkp         = oDKP.getDKP(name)
     return dkp.__str__() + " DKP for " + name
 
 # ADMIN COMMANDS
 def exEditAuc(cmd: botCommand) -> str:
-    id = cmd.regMatch.group("aucId")
-    cmdType = cmd.regMatch.group("cmdType")
-    duration = cmd.regMatch.group("duration")
+    id          = int(cmd.regMatch.group("aucId"))
+    cmdType     = cmd.regMatch.group("cmdType")
+    duration    = cmd.regMatch.group("duration")
     
     return "Auctions do not exist yet therefore i cannot " + cmdType + " them"
 
 def exNewAuc(cmd: botCommand) -> str:
-    switchOpts = cmd.regMatch.group("switchOpts")
-    itemsStr = cmd.regMatch.group("items")
+    switchOpts  = cmd.regMatch.group("switchOpts") if cmd.regMatch.group("switchOpts") is not None else ""
+    itemsStr    = cmd.regMatch.group("items")
     # items = itemsStr.split(|)    
-    duration = cmd.regMatch.group("duration")
-    quanity = cmd.regMatch.group("quanity")
+    duration    = int(cmd.regMatch.group("duration")) if cmd.regMatch.group("duration") is not None else 3
+    quanity     = int(cmd.regMatch.group("quanity")) if cmd.regMatch.group("quanity") is not None else 1
 
-    return "Can't start new auction yet on " + itemsStr + " but options would be " + switchOpts + " duration " + duration + " quantity " + quanity
+    return "Can't start new auction yet on " + itemsStr + " but options would be " + switchOpts + " duration " + duration.__str__() + " quantity " + quanity.__str__()
 
 def exChan(cmd: botCommand) -> str:
     global AuctionChannel
@@ -79,14 +80,14 @@ def reply(cmd: botCommand, message: str):
     return
 
 def execute(cmd: botCommand):
-    for command in cmdRegistration["Normal"]:
-        if regexHelper.eqCommand(command, cmd):
-            reply(cmd, cmdRegistration["Normal"][command](cmd))
-            return
     if(cmd.Channel == AdminChannel):
         for command in cmdRegistration["Admin"]:
             if regexHelper.eqCommand(command, cmd):
                 reply(cmd, cmdRegistration["Admin"][command](cmd))
                 return
+    for command in cmdRegistration["Normal"]:
+        if regexHelper.eqCommand(command, cmd):
+            reply(cmd, cmdRegistration["Normal"][command](cmd))
+            return
     return
 
