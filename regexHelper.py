f6f870df1710d443967aa4ca84f6342a62ad3275
Ryan Johnsonbaugh <ryan.johnsonbaugh@gmail.com>
2022-08-02 22:59:13 -0400

    fixed match groups for aucIdCmdsPtrn

--- regexHelper.py
+++ regexHelper.py
@@ -2,7 +2,7 @@ import re
 from botCommand import botCommand
 commandPattern = re.compile("\[[\w\d: ]+\]\s(\w+)\stells\s(the\s)?(\w+:)?(group|guild|raid|you|\d+),\s'\w*!(.*)'")
 aucCmdPtrn = re.compile("^\s*auc\s+((?P<switchOpts>-[sa]+)\s*)?((?P<items>[^\n\r@]+[^\s@])\s*)(@\s*(?P<duration>\d+\.?\d*)(\s+(?P<quanity>\d+))?)?")
-aucIdCmdsPtrn = re.compile("(?i)^\s*(?P<aucId>\d+)\s+(?P<cmdType>award|pause|close|start)\s*((?P<duration>\d+\.?\d*)\s+((?P<quanity>\d+)\s+(?P<autoAward>0|1|true|false)?)?)?")
+aucIdCmdsPtrn = re.compile("(?i)^\s*(?P<aucId>\d+)\s+(?P<cmdType>award|pause|close|start)\s*(?P<duration>\d+\.?\d*)?(\s+(?P<quanity>\d+)?)?(\s+(?P<autoAward>0|1|true|false)?)?")
 bidWithIDPtrn = re.compile("^\s*(?P<aucId>\d+)\s+(?P<bidVal>\d+)(\s+(?P<bidMax>\d+))?(\s+(?P<bidInc>-?[\d]+))?(\s*(?P<proxyToon>\w+))?")
 bidWithItemPtrn = re.compile("^\s*(?P<aucItem>[^\n\r@]+?)\s*@\s*(?P<bidVal>\d+)(\s+(?P<bidMax>\d+))?(\s+(?P<bidInc>-?[\d]+))?(\s*(?P<proxyToon>\w+))?")
 
